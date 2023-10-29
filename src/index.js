159faafe64641de7a8ae8e1b207fdbfb4a08e2be
Svetlaya87 <svetlaya87@gmail.com>
2023-10-29 20:51:27 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!matrix){
+        return [];
+    }
+    
+    let res = matrix.map((el,i)=> {
+        if(i%2 !==0){
+            el = el.sort((a,b)=> b-a);
+        }
+        return el;
+    });
+    console.log('Console'+res+typeof res);
+    res = res.flat(Infinity);
+    return res;
+  
 }
